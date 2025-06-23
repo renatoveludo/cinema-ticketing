5d05879a7db518a3ced7814ccacd63506ba695da
renatoveludo <matiasveludo@gmail.com>
2025-06-23 16:57:36 -0300

    change PORT backend

--- backend/index.js
+++ backend/index.js
@@ -296,7 +296,10 @@ app.delete('/cleanup-empty-sessions/:cpf', async (req, res) => {
   }
 });
 
-app.listen(4000, () => {
-  console.log('Server is running on http://localhost:4000');
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
